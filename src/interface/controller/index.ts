2332a37e3c39e49b7a46906cccc8064076bc83de
ASpooky <sysk-s0307@docomo.ne.jp>
2026-01-13 23:50:05 +0900

    feat: interface層を追加しました

---
+++ src/interface/controller/index.ts
@@ -0,0 +1,8 @@
+import type { Request,Response } from "express";
+
+const IndexController = (req:Request,res:Response) =>{
+    //　ここでリクエストからパラメータなどをひきだしたりする
+    // usecaseを呼び出したりする
+    // preseterを呼び出したりする
+    // viewModelを返す。
+}
